1111380e45ba145cac01a1ad3162b89d745f0956
Omar Rojas Rubino <orojas@heuristyc.com>
2024-07-18 18:12:21 -0300

    adding card state

--- Scripts/card.gd
+++ Scripts/card.gd
@@ -1,28 +1,47 @@
-extends Container
+class_name AlchemyCard extends Container
 @onready var animation_player = $AnimationPlayer
 @onready var card_layout = $CardLayout
+@onready var card_copy = preload("res://Scene/card_holder.tscn")
+const card_holder = "Board/CardHolder"
 
-@onready var card = preload("res://Scene/card_holder.tscn")
+enum {
+	ON_HAND,
+	ON_FIELD,
+	ATTACK,
+}
 var cardHighlighted = false
+var state = ON_HAND
 
+func change_state(new_state):
+	state = new_state
+		
 func _on_mouse_entered():
-	animation_player.play("Select")
+	if state == ON_HAND:
+		animation_player.play("Select")
 	cardHighlighted = true
 
 func _on_mouse_exited():
-	animation_player.play("Deselect")
+	if state == ON_HAND:
+		animation_player.play("Deselect")
 	cardHighlighted = false
 
 # Esto maneja de manera individual los eventos que se hagan en cada carta
 func _on_gui_input(event):
+	match state:
+		ON_HAND: 
+			createCopy(event)
+		ON_FIELD:
+			move_card(event)
+		ATTACK:
+			pass
+		_: 
+			pass
+
+func createCopy(event):
 	if (event is InputEventMouseButton) and (event.button_index == 1):
 		if event.button_mask == 1:
 			#press down
-			if cardHighlighted:
-				var cardTemp = card.instantiate()
-				get_tree().get_root().get_node("Board/CardHolder").add_child(cardTemp)
-				Game.cardSelected = true
-				#card_layout.hide()
+			card_on_heand()
 		elif event.button_mask == 0:
 			#Place the card outside board
 			if !Game.mouseOnPlacement:
@@ -30,8 +49,32 @@ func _on_gui_input(event):
 				card_layout.show()
 			else:
 				#Place the card on board
-				self.queue_free()
-				get_node("../../AlchemyPlacement").placeCard()
-			for i in get_tree().get_root().get_node("Board/CardHolder").get_child_count():
-					get_tree().get_root().get_node("Board/CardHolder").get_child(i).queue_free()
+				#self.queue_free()
+				get_node("../../AlchemyPlacement").placeCard(self)
+			for i in get_tree().get_root().get_node(card_holder).get_child_count():
+					get_tree().get_root().get_node(card_holder).get_child(i).queue_free()
 			Game.cardSelected = false
+			
+func move_card(event):
+	if (event is InputEventMouseButton) and (event.button_index == 1):
+		if event.button_mask == 1:
+			#press down
+			card_on_heand(true)
+	elif event.button_mask == 0:
+			#Place the card outside board
+			if !Game.mouseOnPlacement:
+				cardHighlighted = false
+				card_layout.show()
+			else:
+				get_node("../../AlchemyPlacement").placeCard(self)
+			for i in get_tree().get_root().get_node(card_holder).get_child_count():
+					get_tree().get_root().get_node(card_holder).get_child(i).queue_free()
+			Game.cardSelected = false
+
+func card_on_heand(hide = false):
+	if cardHighlighted:
+		var cardTemp = card_copy.instantiate()
+		get_tree().get_root().get_node(card_holder).add_child(cardTemp)
+		Game.cardSelected = true
+		if hide:
+			card_layout.hide()
--- Scripts/card_placement.gd
+++ Scripts/card_placement.gd
@@ -1,5 +1,10 @@
 extends BoxContainer
-@onready var CARD_ON_BOARD = preload("res://Scene/card_on_board.tscn")
+
+enum {
+	ON_HAND,
+	ON_FIELD,
+	ATTACK,
+}
 
 func _on_mouse_entered():
 	Game.mouseOnPlacement = true
@@ -8,9 +13,14 @@ func _on_mouse_entered():
 func _on_mouse_exited():
 	Game.mouseOnPlacement = false
 
-func placeCard():
-	var card = CARD_ON_BOARD.instantiate()
-	var projectResolution = ProjectSettings.get_setting("display/window/size/viewport_width")
-	var projectResolitionHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
-	card.global_position = Vector2(projectResolution/2, projectResolitionHeight/2) - self.position
-	add_child(card)
+func placeCard(card: AlchemyCard):
+	var state = card.state
+	if state == ON_HAND:
+		var duplicated = card.duplicate()
+		duplicated.state = ON_FIELD
+		#var projectResolution = ProjectSettings.get_setting("display/window/size/viewport_width")
+		#var projectResolitionHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
+		#duplicated.global_position = Vector2(projectResolution/2, projectResolitionHeight/2) - self.position
+		add_child(duplicated)
+	elif state == ON_FIELD:
+		card.card_layout.show()
